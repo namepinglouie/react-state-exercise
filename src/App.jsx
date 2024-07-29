f1b1b479b922840bf75629d906b03dc325b99ecb
Name Ping Louie <namepinglouie@gmail.com>
2024-07-29 11:28:16 -0400

    edit App.jsx file by adding counter button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleIncrease5Click() {
+    setCounter(counter + 5);
+  }
+  function handleDecrease2Click() {
+    setCounter(counter - 2);
+  }
 
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <div>{counter}</div>
+      <button onClick = {handleIncrease5Click}>+5</button>
+      <button onClick = {handleDecrease2Click}>-2</button>
+    </div>
   )
 }
 
